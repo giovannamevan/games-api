377ce29a37309fb8a21135742e9f516e71432c98
Giovanna Maria Alves Evangelista <giovannaevangelista78@gmail.com>
2023-05-17 09:43:32 -0300

    Search game by Id

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 	
@@ -19,7 +20,15 @@ public class GameMinDTO {
 		this.imgUrl = entity.getImgUrl();
 		this.shortDescription = entity.getShortDescription();
 	}
-
+	
+	public GameMinDTO(GameMinProjection entity) {
+		
+		this.id = entity.getId();
+		this.title = entity.getTitle();
+		this.gameYear = entity.getYear();
+		this.imgUrl = entity.getImgUrl();
+		this.shortDescription = entity.getShortDescription();
+	}
 
 	public Long getId() {
 		return id;
